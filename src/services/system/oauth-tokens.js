c03330a6b781481c1f4b74a8702e2383d901f7ba
ArturoVRZ <arturo.villalobos@cceo.com.mx>
2024-03-14 12:39:59 -0600

    feat (profile and 2fa services) :sparkles: added all services for 2fa and profile info

--- src/services/system/oauth-tokens.js
+++ src/services/system/oauth-tokens.js
@@ -28,3 +28,45 @@ export function changepassword(email) {
         email: email
     })
 }
+
+export function getQrCode(email, type) {
+    return service.get('/two-factor-qr-code',{
+        grant_type: 'password',
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        email: email,
+        type: type,
+    });
+}
+
+export function sendCode2FA(data) {
+    return service.post('verificar-codigo-2fa',{
+        grant_type: 'password',
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        code: data.code,
+        email: data.email,
+        password: data.password,
+    })
+}
+export function disable2FA(data) {
+    return service.delete('users-disable2FA',{
+        code: data.code,
+    })
+}
+
+export function enable2FA() {
+    return service.post('users-enable2FA')
+}
+
+export function status2FA() {
+    return service.get('users-2FA-status')
+}
+
+export function reset2FAuser(id) {
+    return service.post('/reset-2FA/' + id, {
+        grant_type: 'password',
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+    })
+}
