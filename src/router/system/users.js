b73ccffd717c14c0bbfa89d23b4c04b561d372e0
ArturoVRZ <arturo.villalobos@cceo.com.mx>
2024-03-26 17:22:31 -0600

    fix (user view) :bug: deleted users view

--- src/router/system/users.js
+++ src/router/system/users.js
@@ -1,12 +1,6 @@
-import Users from "@/views/system/Users.vue";
 import Profile from "@/views/system/Profile.vue";
 
 export default [
-	{
-		path: '/usuarios',
-		name: 'users',
-		component: Users,
-	},
 	{
 		path: '/perfil',
 		name: 'profile',
